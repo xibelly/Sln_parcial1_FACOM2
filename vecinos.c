9ff0a2a13bb8208ccccb8e56c8428eedb021efd5
xibelly <xibeline@gmail.com>
2016-09-22 16:05:09 -0500

    Add files via upload

--- vecinos.c
+++ vecinos.c
@@ -2,6 +2,14 @@
 Xibelly Eliseth Mosquera Escobar
 
 Solucion Parcial I COMPUTACION CIENTIFICA AVANZADA
+
+Para solucionar el parcial, mas especificamnete para calcular los vecinos tenemos dos modos de operacion:
+
+Modo1) Malla
+
+Modo2) Fuerza bruta
+
+
  */
 
 
@@ -17,50 +25,37 @@ Nbox = 25
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<time.h>
 
 #include <gsl/gsl_sort_int.h>
 #include <gsl/gsl_sort_double.h>
+
 
 #include <distancia.h>
 
 //////VARIABLE GLOBALES//////
 
-int Nparticles, Nboxes;
-double Lbox;
+clock_t tini, tend, tacum;
+double cpu_time_used;
+
+int Nparticles;
 double *dist;
 
 ///////intento
 
-FILE *pf=NULL;
+
 FILE *pf2=NULL;
+FILE *pf3=NULL;
+FILE *pf4=NULL;
 
 struct particle   
 {
   double pos[2];
-  
+  double x, y;
+  int id;
   
 }; 
 struct particle *part; 
-
-struct malla   
-{
-  double *X, *Y;  
-  double *pos_x;
-  double *pos_y;
-  double *centro_x;
-  double *centro_y;
-  int *part_cells;
-  
-}; 
-struct malla grid;
-
-struct esquina
-{
-  double x;
-  double y;
-
-};
-struct esquina *Esquina;
 
 
 ////////////////////////////////////
@@ -75,27 +70,26 @@ struct esquina *Esquina;
 
 int main(int argc, char **argv) 
 {
-  int n, i, j, l, m;
-  int index;
-  int Nesquinas;
+  int  i, j, nread, c, id_i, id_j;
   int suma;
+  double a, b;
+
   
   char *filename;
-  double box_delta;
-  double delta;
 
-  double *dist_grid;
   double distmin, distmax;
   double X_centro, Y_centro;
+  double r_xi, r_yi, r_xj, r_yj;
   double xi, yi, xj, yj;
+  double R;
   
 
   printf("%d\n",argc);
 
-  if(argc != 5)
+  if(argc != 3)
     {
       printf("ERROR--> use as:\n");
-      printf("%s Npartilces Lbox Nboxes datafile\n",argv[0]);
+      printf("%s Npartilces datafile\n",argv[0]);
       exit(0);  
     }
 
@@ -103,46 +97,17 @@ int main(int argc, char **argv)
 
   Nparticles = atoi(argv[1]);
   
-  Lbox       = atof(argv[2]);   
-    
-  Nboxes     = atoi(argv[3]);   
+  filename   = argv[2];
 
-  filename   = argv[4];
+  printf("%s %d %s\n",argv[0], Nparticles, filename);
 
-  printf("%s %d %lf %d %s\n",argv[0], Nparticles, Lbox, Nboxes, filename);
-
-  box_delta = Lbox/(1.0*Nboxes); 
-
-  delta = 10* box_delta; //tamaño de las celda
   
-  n = Nboxes*Nboxes;
-
-  Nesquinas = (4*n);
-
-  grid.X = (double*) malloc(n *sizeof(double));  //malla
-
-  grid.Y = (double *) malloc(n *sizeof(double));
-
-  grid.pos_x = (double *) malloc(n *sizeof(double));
-
-  grid.pos_y = (double *) malloc(n *sizeof(double));
-
-  grid.centro_x = (double *) malloc(n *sizeof(double));
-
-  grid.centro_y = (double *) malloc(n *sizeof(double));
-
-  dist_grid = (double *) malloc(Nparticles *sizeof(double));
-
-  grid.part_cells = (int *) malloc(Nparticles *sizeof(int));
-
-  Esquina = (struct esquina *) malloc((size_t) Nesquinas* sizeof(struct esquina));
   
-
-
   dist = (double *) malloc(Nparticles *sizeof(double));  //particulas
 
   part = (struct particle *) malloc(Nparticles *sizeof(struct particle));
 
+  //part.id = (int *) malloc(Nparticles *sizeof(int));
   
 
   size_t p[Nparticles];
@@ -154,96 +119,95 @@ int main(int argc, char **argv)
   
   read_file(filename, Nparticles);
 
-  pf2 = fopen("coordenadas2.dat","w");
+  //pf2 = fopen("coordenadas2.dat","w");
 
   for(i=0; i<Nparticles; i++)//lectura
     {
-      fprintf(pf2,"%lf %lf\n",part[i].pos[0], part[i].pos[1]);
+      //fprintf(pf2,"%lf %lf\n",part[i].pos[0], part[i].pos[1]);
     }
 
   
-  /* Construir el grid*/
-
-  pf = fopen("malla.dat","w");
-
-  for (l=0; l<Nboxes;l++)
-    {
-      for (j=0; j<Nboxes; j++)
-	{
-	  
-	  index = Nboxes * l + j;
-	  //printf("%d\n",index);
-	  grid.X[index]= l * box_delta;
-	  grid.Y[index] = j * box_delta;
-	  
-	  fprintf(pf,"%lf %lf\n",grid.X[index], grid.Y[index]);
-	}
-	
-    }
-
-  fclose(pf);
-
-  
-  /* Llenamos el grid*/
-  suma = 0;
-  
-  for (l=0; l<Nboxes;l++)
-    {
-      for (j=0; j<Nboxes; j++)
-	{
-	  index = Nboxes * l + j;
-	  
-	  //printf("%d\n",index); //centro de las celdas
-
-	  //grid.centro_y[index] = distancia(grid.X[index], grid.X[index+1], grid.Y[index], grid.Y[index+1]);
-
-	  //printf("%lf\n", grid.centro_y[index]);
-	  //grid.centro_x[index] = distancia(grid.X[index], grid.X[index+1], grid.Y[index], grid.Y[index+1]);
-
-	  
-	  /*for(m=Nboxes; m<Nesquinas; m++)
-	    {
-	      
-	      Esquina[m].x = grid.X[index];
-	      
-	      Esquina[m].y = grid.Y[index];
-	      
-	      Esquina[m+1].x = grid.X[index+1];
-	      
-	      Esquina[m+1].y = grid.Y[index+1];
-	      
-	      grid.centro_x[m] = distancia(Esquina[m].x, Esquina[m+1].x, Esquina[m].y, Esquina[m+1].y);
-	      
-	      grid.centro_y[m] = distancia(Esquina[m].x, Esquina[m+1].x, Esquina[m].y, Esquina[m+1].y);
-
-	      printf("%lf %lf\n", grid.centro_x[m], grid.centro_y[m]);
-	  
-	      }*/
-	  for(i=0; i<Nparticles; i++)
-	    {
-	      if((part[i].pos[0] >= grid.X[index]) && (part[i].pos[0] <= grid.X[index+Nboxes]))
-		{
-		  
-		  if((part[i].pos[1] >= grid.Y[index]) && (part[i].pos[1] <= grid.Y[index+1]))
-		    {
-		      suma = suma + 1;  
-		    }
-		  
-		}
-
-	      grid.part_cells[index] = suma;
-	      //printf("particles/cell[%d]: %d\n", index, grid.part_cells[index]);
-	    }
-	  
-	}
-    }
-
-  printf("%lf %lf %d\n", grid.X[624], grid.X[624],  grid.part_cells[624]);
   
   /*Calculamos los vecinos -los 2 mas cercanos- */
 
+  tini = clock();
+  
+  for(i=0; i<Nparticles; i++)
+    {
+        for(j=0; j<Nparticles; j++)
+	  {
+	    if(j != i)
+	      {
+		r_xi = part[i].pos[0];
+		r_yi = part[i].pos[1];
 
+		r_xj = part[j].pos[0];
+		r_yj = part[j].pos[1];
+
+		dist[j] = distancia(r_xi, r_xj, r_yi, r_yj);
+
+	      }
+	    part[i].id = i;
+
+	  }
+	
+    }
+
+  tend = clock();
+  
+  cpu_time_used = ((double) (tend - tini)) / CLOCKS_PER_SEC;
+
+  printf("CPU TIME USED TO FIND NEIGHBORS: %g\n",cpu_time_used);
+  
+  gsl_sort_smallest_index (p, k, dist, 1, Nparticles);//Se ordenan las particulas en forma creciente
+
+  
+  //pf3 = fopen("vecinos.dat","w");
+  
+  for(i=0; i<Nparticles; i++)
+    {
+      for(j=0; j<Nparticles; j++)
+	  {
+
+	    //fprintf(pf3,"%d %d %lf %lf %lf\n", part[i].id, part[p[j]].id, part[p[j]].pos[0], part[p[j]].pos[1], dist[p[j]]);
+	  }
+    }
+  fclose(pf3);
+  
+  pf4 = fopen("vecinos.dat","r");
+
+  for(i=0; i<Nparticles*Nparticles; i++)
+    {
+      nread = fscanf(pf4,"%d %d %lf %lf %lf", &id_i, &id_j, &part[i].x, &part[i].y, &R);
+
+      c = i+Nparticles;
+      while(c)
+	{
+	  printf("PARTICLE %d HAS 1 TRIANGLES\n",part[i].id);
+	  printf("WITH COORDINATES: \n");  
+	  printf("%lf %lf\n",part[c].x, part[c].y);
+	}
+    }
+  
+  /*for(i=0; i<Nparticles*; i++) //dos vecinos mas cercanos
+    {
+      for(j=0; j<Nparticles; j++)
+	  {
+	    //c =1* (j * Nparticles);
+	    printf("%d\n",c);
+	    printf("PARTICLE %d HAS 1 TRIANGLES\n",part[i].id);
+	    printf("WITH COORDINATES: \n");  
+	    printf("%lf %lf\n",part[p[j]].pos[0], part[p[j]].pos[1]);
+          }
+	
+	  }*/
+  
+  free(dist);
+    
+  exit(0);
+  
   /*Distancia  al centro del sistema */
+
   
   for(i=0; i<Nparticles; i++)
     {
@@ -254,13 +218,14 @@ int main(int argc, char **argv)
   
   free(dist);
   
+  
   /* Coordenadas del centro geometrico*/
 
   X_centro =  part[p[0]].pos[0];
 
   Y_centro =  part[p[0]].pos[1];
 
-  printf("coordenadas del centro\n");
+  printf("CENTER COORDINATES\n");
 
   printf("Xc: %lf Yc: %lf\n", X_centro, Y_centro);
   
@@ -296,15 +261,18 @@ int main(int argc, char **argv)
 
       //write_file("distancia_centro.dat", Nparticles, part, dist);
     }
-  printf("Valor min de dist %.9lf\n",distmin);
-  printf("Valor max de dist: %.9lf\n",distmax);
-
+  printf("MIN VALUE OF DIST %.9lf\n",distmin);
+  printf("MAX VALUE OF DIST: %.9lf\n",distmax);
+ 
   
   
   
   
   /* Imprimir resultados y salir */
 
+
+  //fclose(pf2);
+  
   return 0;
 
 }
